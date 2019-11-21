cf103a048e05dcda7721f0c13e93d6c3bd8d69cd
t-kuha <imagingtechnerd@gmail.com>
2019-11-21 03:48:05 +0000

    Add create_pfm.tcl

---
+++ accel/create_pfm.tcl
@@ -0,0 +1,20 @@
+# Create acceleration platform
+
+platform create -name {u96_accel} -hw {ultra96.xsa} -proc {psu_cortexa53} -os {linux} -no-boot-bsp -out {_pfm}
+platform write
+# platform read {/home/pentaxmedical/vitis-workspace/u96_accel/platform.spr}
+platform active {u96_accel}
+
+# Add sysroot??
+domain create -name xrt -os linux -proc psu_cortexa53
+domain active xrt
+domain config -bif {src/linux.bif}
+domain config -boot {petalinux/images/linux}
+domain config -image {petalinux/images/linux}
+domain config -qemu-data {petalinux/images/linux}
+domain config -qemu-args {src/qemu_args.txt}
+domain config -pmuqemu-args {src/pmu_args.txt}
+platform write
+
+platform generate
+
