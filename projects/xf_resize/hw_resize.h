c7f2387a0c54c203885f7b67e6b8bfbb93b2409d
t_kuha <imagingtechnerd@gmail.com>
2019-12-25 19:35:05 +0900

    Update xf_resize project
    Reduce memory copy in PS

--- projects/xf_resize/hw_resize.h
+++ projects/xf_resize/hw_resize.h
@@ -6,9 +6,14 @@
 #define 	_DST_COLS           (_SRC_COLS/2)
 #define     _MAX_DOWN_SCALE     2
 
+#define		_NPC				XF_NPPC1
+#define 	_TYPE				XF_8UC3
 
 typedef	unsigned int		data_t;
 
-extern "C" {
-void resize(data_t* img_in, data_t* img_out);
-}
+// Enable this when using HW function
+#define USE_OCL
+
+#ifndef USE_OCL
+void hw_resize(data_t* img_in, data_t* img_out);
+#endif
